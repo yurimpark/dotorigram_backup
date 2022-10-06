081febf4f98f23e363d97689313ec946532820f1
khjoo1203 <khjoo1203@naver.com>
2022-10-06 16:27:35 +0900

    css 로그인 회원가입 수정2

--- src/components/Login/Register.jsx
+++ src/components/Login/Register.jsx
@@ -2,14 +2,32 @@ import React from 'react';
 
 const Register = () => {
   return (
-    <div>
-      <form>
-        <input type="text" placeholder="휴대폰 번호 또는 이메일 주소" />
-        <input type="text" placeholder="성명" />
-        <input type="text" placeholder="사용자 이름" />
-        <input type="password" placeholder="비밀번호" />
-        <input type="submit" value="가입" />
-      </form>
+    <div class="flex flex-col h-screen justify-center items-center">
+      <div class="w-[21.875rem]">
+        <form class="flex flex-col">
+          <input
+            class="bg-[#fafafa] pt-3.5 pr-0 py-0.5 px-2"
+            type="text"
+            placeholder="휴대폰 번호 또는 이메일 주소"
+          />
+          <input
+            class="bg-[#fafafa] pt-3.5 pr-0 py-0.5 px-2"
+            type="text"
+            placeholder="성명"
+          />
+          <input
+            class="bg-[#fafafa] pt-3.5 pr-0 py-0.5 px-2"
+            type="text"
+            placeholder="사용자 이름"
+          />
+          <input
+            class="bg-[#fafafa] pt-3.5 pr-0 py-0.5 px-2"
+            type="password"
+            placeholder="비밀번호"
+          />
+          <input class="bg-[#0095f6] text-white" type="submit" value="가입" />
+        </form>
+      </div>
     </div>
   );
 };
