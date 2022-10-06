8b33660bdf31f2ece236d01ad7682c6e57df1c22
yurimpark <99720055+yurimpark@users.noreply.github.com>
2022-10-06 15:55:44 +0900

    ContentUpload 수정, PostedContent 생성

--- src/components/main/FeedContent.jsx
+++ src/components/main/FeedContent.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import { AiOutlineHeart } from 'react-icons/ai';
-import { TbMessageCircle2 } from 'react-icons/tb';
+import { AiOutlineHeart } from "react-icons/ai";
+import { TbMessageCircle2 } from "react-icons/tb";
 const FeedContent = () => {
   return (
-    <div className='mx-3'>
+    <div className="mx-3">
       <div>
         <button>
-          <AiOutlineHeart className='text-xl' />
+          <AiOutlineHeart className="text-xl" />
         </button>
         <button>
-          <TbMessageCircle2 className='text-xl' />
+          <TbMessageCircle2 className="text-xl" />
         </button>
       </div>
       <div>좋아요 갯수</div>
-      <div className='flex gap-10'>
+      <div className="flex gap-10">
         <div>아이디</div>
         <div>내용~~~~</div>
       </div>
--- src/components/main/FeedList.jsx
+++ src/components/main/FeedList.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ContentPhoto from './ContentPhoto';
-import FeedContent from './FeedContent';
+import React from "react";
+import ContentPhoto from "./ContentPhoto";
+import FeedContent from "./FeedContent";
 const FeedList = () => {
   return (
     <div>
-      <div className='flex justify-around  mx-3'>
-        <h2 className='basis-1/8'>아이콘</h2>
-        <div className='basis-1/2'>닉네임</div>
-        <div className='flex justify-around basis-1/3'>
-          <button className='basis-1/2'>수정</button>
-          <button className='basis-1/2'>삭제</button>
+      <div className="flex justify-around  mx-3">
+        <h2 className="basis-1/8">아이콘</h2>
+        <div className="basis-1/2">닉네임</div>
+        <div className="flex justify-around basis-1/3">
+          <button className="basis-1/2">수정</button>
+          <button className="basis-1/2">삭제</button>
         </div>
       </div>
       <ContentPhoto />
