f28e7cfab99e9b93482dc67165694ad9ef28bdbb
khjoo1203 <khjoo1203@naver.com>
2022-10-06 10:12:42 +0900

    css 로그인 수정

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,10 +6,10 @@ const Login = () => {
   return (
     <div>
       <Link to="/"><img style={{width:175}} src={logo} alt="" /></Link>
-      <form class="flex-col">
-        <input class="bg-black min-w-full h-9 pt-3.5 pr-0 py-0.5 px-2" type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
-        <input type="password" placeholder="비밀번호" />
-        <input type="submit" id="logins" value="로그인" />
+      <form class="flex flex-col">
+        <input class="w-64 h-9 pt-3.5 pr-0 py-0.5 px-2" type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
+        <input class="w-64 h-9 pt-3.5 pr-0 py-0.5 px-2" type="password" placeholder="비밀번호" />
+        <input class="w-64 h-4.5" type="submit" id="logins" value="로그인" />
       </form>
       <span>계정이 없으신가요? <Link to="/register">가입하기</Link></span>
     </div>
