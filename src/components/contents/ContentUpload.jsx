dac6d062289364a95fa750af1637c93f09ecaf9a
yurimpark <99720055+yurimpark@users.noreply.github.com>
2022-10-11 15:40:38 +0900

    ContentUpload 조금 수정

--- src/components/contents/ContentUpload.jsx
+++ src/components/contents/ContentUpload.jsx
@@ -5,9 +5,13 @@ function ContentUpload() {
   return (
     <div className="mx-auto max-w-470">
       <div className="flex justify-between mt-2">
-        <CloseIcon className="ml-5" />
+        <button>
+          <CloseIcon className="ml-5" />
+        </button>
         <span>새로운 사진 게시물</span>
-        <span className="text-blue-500 font-bold mr-5">공유하기</span>
+        <button>
+          <span className="text-blue-500 font-bold mr-5">공유하기</span>
+        </button>
       </div>
       <img
         src={require("../../static/img/feed_img.jpg")}
@@ -15,7 +19,7 @@ function ContentUpload() {
         alt="img"
       />
       <textarea
-        className="border-none w-full h-96 text-xl placeholder:text-base"
+        className="outline-none w-full h-96 text-xl placeholder:text-base"
         placeholder="문구 입력..."
       ></textarea>
     </div>
