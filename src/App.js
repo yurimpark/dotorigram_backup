a8269bdd34dd51645b8eab10ab189f263cd1fbca
khjoo1203 <khjoo1203@naver.com>
2022-10-13 10:28:46 +0900

    마이페이지 로그인 유무

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import * as React from 'react';
+import React, {useState} from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Mainpage from './pages/Mainpage';
 import Login from './pages/Login';
@@ -8,17 +8,19 @@ import ContentUpload from './pages/ContentUpload';
 import PostedContent from './pages/PostedContent';
 import Mypage from './pages/Mypage';
 import CommentsBox from './pages/CommentsBox';
+import PrivateRoute from './route/PrivateRoute';
 
 function App() {
+  const [authenticate, setAuthenticate] = useState(false); //true면 로그인 됨, false면 로그인 안됨
   return (
     <Routes>
       <Route path='/' element={<Mainpage />} />
-      <Route path='/login' element={<Login />} />
+      <Route path='/login' element={<Login setAuthenticate={setAuthenticate} />} />
       <Route path='/register' element={<Register />} />
       <Route path='/contentupload' element={<ContentUpload />} />
       <Route path='/postedcontent' element={<PostedContent />} />
       <Route path='/commentsbox' element={<CommentsBox />} />
-      <Route path='/mypage' element={<Mypage />} />
+      <Route path='/mypage' element={<PrivateRoute authenticate={authenticate} />} />
     </Routes>
   );
 }
